702ef8a8c343d55494ebe366fcc7eca28bc41166
ThomasFzr <thfltzr@gmail.com>
2024-05-03 17:18:31 +0200

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pokemonRouter = require('./routes/pokemons');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use('/api', pokemonRouter);
